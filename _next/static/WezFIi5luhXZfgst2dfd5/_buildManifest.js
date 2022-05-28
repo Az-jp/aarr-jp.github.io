4cf1644e748bbae0778c1f81773cbb6a03872641
aarr-jp <aarr-jp@users.noreply.github.com>
2022-05-28 18:54:49 +0000

    deploy: 1f1c0b3f40b081eefe61eb4b3b2a3c6ee7d9ac21

--- _next/static/WezFIi5luhXZfgst2dfd5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/766-0c306f90789966f1.js","static/chunks/pages/index-8be681a723148272.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
